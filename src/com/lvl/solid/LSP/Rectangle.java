b7a19dd0e2e8d55e414c6520520cafbb640b162c
LVL <luis.vinelerma@yahoo.com>
2023-04-30 14:54:45 +0200

    Add a rectangle class

---
+++ src/com/lvl/solid/LSP/Rectangle.java
@@ -0,0 +1,42 @@
+package com.lvl.solid.LSP;
+
+public class Rectangle {
+
+	protected int width, height;
+
+	public Rectangle() {
+		super();
+	}
+	
+	public Rectangle(int width, int height) {
+		super();
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getArea() {
+		return width * height;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Rectangle [width=%s, height=%s]", width, height);
+	}
+
+}
