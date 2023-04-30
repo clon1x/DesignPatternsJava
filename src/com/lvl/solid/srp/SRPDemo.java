a176299f721684883133a5bdec8ac948093a50d4
LVL <luis.vinelerma@yahoo.com>
2023-04-30 12:53:06 +0200

    Add a journal class

---
+++ src/com/lvl/solid/srp/SRPDemo.java
@@ -0,0 +1,13 @@
+package com.lvl.solid.srp;
+
+public class SRPDemo {
+
+	public static void main(String[] args) {
+		Journal j = new Journal();
+		j.addEntry("I cried today");
+		j.addEntry("I ate a bug");
+		
+		System.out.println(j);
+	}
+
+}
