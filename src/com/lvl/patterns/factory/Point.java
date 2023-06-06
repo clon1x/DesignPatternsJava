354477b3b219dadf27efe94cc1623a8096b4dfd8
LVL <luis.vinelerma@telefonica.com>
2023-06-06 13:44:28 +0200

    Create Factory Methods
    Factory methods allow different "constructors" each one with appropiate method and parameters names.

--- src/com/lvl/patterns/factory/Point.java
+++ src/com/lvl/patterns/factory/Point.java
@@ -4,26 +4,51 @@ public class Point {
 
 	private double x, y;
 	
-	public enum CoordinateSystem {
-		CARTESIAN, POLAR
+	private Point(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
 	
-	/**
-	 * Cartesian or polar coordinates
-	 * @param a - x coordinate, or rho
-	 * @param b - y coordinate, or theta
-	 */
-	public Point(double a, double b, CoordinateSystem cs) {
-		switch (cs) {
-			case CARTESIAN:
-				this.x = a;
-				this.y = b;
-				break;
-			
-			case POLAR:
-				this.x = a * Math.cos(b);
-				this.y = a * Math.sin(b);
-				break;
-		}
+	public static Point newCartesianPoint(double x, double y) {
+		return new Point(x, y);
 	}
+	
+	public static Point newPolarPoint(double rho, double theta) {
+		return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public double getRho() {
+		return Math.sqrt(x*x + y*y);
+	}
+	
+	public double getTheta() {
+		return Math.atan2(y, x);
+	}
+	
+	public String toString(int precision) {
+		double pot10 = Math.pow(10,precision);
+		StringBuilder builder = new StringBuilder();
+		builder.append("Point [x=");
+		builder.append(Math.round(x * pot10) / pot10);
+		builder.append(", y=");
+		builder.append(Math.round(y * pot10) / pot10);
+		builder.append("]");
+		return builder.toString();		
+	}
+
+	@Override
+	public String toString() {
+		return toString(18);
+	}
+	
+	
+	
 }
---
+++ src/com/lvl/patterns/factory/clients/PointDemo.java
@@ -0,0 +1,18 @@
+package com.lvl.patterns.factory.clients;
+
+import com.lvl.patterns.factory.Point;
+
+public class PointDemo {
+
+	public static void main(String[] args) {
+
+		Point a = Point.newCartesianPoint(3, 4);
+		
+		double theta = 0.92729521800161223242851246292243;
+		Point b = Point.newPolarPoint(5, theta);
+
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+	}
+
+}
