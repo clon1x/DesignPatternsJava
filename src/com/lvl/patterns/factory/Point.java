22545489937b2684cbf51f3d50506d7865c31092
LVL <luis.vinelerma@telefonica.com>
2023-06-06 13:45:22 +0200

    Factory as an inner class of Point
    In order to keep Point constructor private, the factory has to be an inner class inside Point.

--- src/com/lvl/patterns/factory/Point.java
+++ src/com/lvl/patterns/factory/Point.java
@@ -9,13 +9,17 @@ public class Point {
 		this.y = y;
 	}
 	
-	public static Point newCartesianPoint(double x, double y) {
-		return new Point(x, y);
-	}
-	
-	public static Point newPolarPoint(double rho, double theta) {
-		return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
-	}
+	public static class Factory {
+		
+		public static Point newCartesianPoint(double x, double y) {
+			return new Point(x, y);
+		}
+		
+		public static Point newPolarPoint(double rho, double theta) {
+			return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+		}
+
+	}	
 	
 	public double getX() {
 		return x;
--- src/com/lvl/patterns/factory/clients/PointDemo.java
+++ src/com/lvl/patterns/factory/clients/PointDemo.java
@@ -6,10 +6,10 @@ public class PointDemo {
 
 	public static void main(String[] args) {
 
-		Point a = Point.newCartesianPoint(3, 4);
+		Point a = Point.Factory.newCartesianPoint(3, 4);
 		
 		double theta = 0.92729521800161223242851246292243;
-		Point b = Point.newPolarPoint(5, theta);
+		Point b = Point.Factory.newPolarPoint(5, theta);
 
 		System.out.println("a: " + a);
 		System.out.println("b: " + b);
