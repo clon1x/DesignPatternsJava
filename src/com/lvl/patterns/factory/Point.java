e54e122d74f3602a711faf5e5b98ae1bd04edb71
LVL <luis.vinelerma@telefonica.com>
2023-06-06 13:47:51 +0200

    PointFactory as a separate class.
    This forces us to make the Point constructor visible, at least with 'Package' visibility.

--- src/com/lvl/patterns/factory/Point.java
+++ src/com/lvl/patterns/factory/Point.java
@@ -4,22 +4,10 @@ public class Point {
 
 	private double x, y;
 	
-	private Point(double x, double y) {
+	Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	public static class Factory {
-		
-		public static Point newCartesianPoint(double x, double y) {
-			return new Point(x, y);
-		}
-		
-		public static Point newPolarPoint(double rho, double theta) {
-			return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
-		}
-
-	}	
 	
 	public double getX() {
 		return x;
--- src/com/lvl/patterns/factory/clients/PointDemo.java
+++ src/com/lvl/patterns/factory/clients/PointDemo.java
@@ -1,15 +1,16 @@
 package com.lvl.patterns.factory.clients;
 
 import com.lvl.patterns.factory.Point;
+import com.lvl.patterns.factory.PointFactory;
 
 public class PointDemo {
 
 	public static void main(String[] args) {
 
-		Point a = Point.Factory.newCartesianPoint(3, 4);
+		Point a = PointFactory.newCartesianPoint(3, 4);
 		
 		double theta = 0.92729521800161223242851246292243;
-		Point b = Point.Factory.newPolarPoint(5, theta);
+		Point b = PointFactory.newPolarPoint(5, theta);
 
 		System.out.println("a: " + a);
 		System.out.println("b: " + b);
