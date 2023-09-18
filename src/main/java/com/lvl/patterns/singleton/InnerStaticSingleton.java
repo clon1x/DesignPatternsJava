7f9468cfb894304cf8f6059c89edffeab43340d5
LVL <luis.vinelerma@telefonica.com>
2023-09-18 13:27:18 +0200

    Inner class singleton
    Using an inner static class that actually implements the singleton instance, we automatically get a thread-safe, lazy implementation of the singleton, which is much simpler than the previous one, in LazySingleton.

---
+++ src/main/java/com/lvl/patterns/singleton/InnerStaticSingleton.java
@@ -0,0 +1,15 @@
+package com.lvl.patterns.singleton;
+
+public class InnerStaticSingleton {
+    
+    private InnerStaticSingleton() {}
+    
+    private static class Impl {
+	private static final InnerStaticSingleton INSTANCE = new InnerStaticSingleton();
+    }
+    
+    public InnerStaticSingleton getInstance() {
+	return Impl.INSTANCE;
+    }
+
+}
