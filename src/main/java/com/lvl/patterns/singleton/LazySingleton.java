bd6e9d29816e3e308bc36c4562ad2e62c46a1834
LVL <luis.vinelerma@telefonica.com>
2023-09-18 13:19:28 +0200

    Double-check singleton initialization (outdated)
    A way of securely instantiating a singleton which is both, lazy, and thread-safe.

--- src/main/java/com/lvl/patterns/singleton/LazySingleton.java
+++ src/main/java/com/lvl/patterns/singleton/LazySingleton.java
@@ -6,11 +6,14 @@ public class LazySingleton {
     
     private LazySingleton() {}
 
-    public static synchronized LazySingleton getInstance() {
+    public static LazySingleton getInstance() {
 	
-	if (instance == null)
-	    instance = new LazySingleton();
-	
+	if (instance == null) {
+	    synchronized(LazySingleton.class) {
+		if (instance == null)
+		    instance = new LazySingleton();
+	    }
+	}
 	return instance;
     }
     
