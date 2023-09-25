da43987842a6fa365c7a67f982fa9a1367ba3820
LVL <luis.vinelerma@telefonica.com>
2023-09-25 14:26:36 +0200

    Static decorator composition
    In this example, the types of all the variables are static.

--- src/test/java/com/lvl/patterns/structural/decorator/shape/ShapeTest.java
+++ src/test/java/com/lvl/patterns/structural/decorator/shape/ShapeTest.java
@@ -15,7 +15,7 @@ class ShapeTest {
     void should_ReturnInfo_When_SimpleCircleShape() {
 	
 	// given
-	Shape circle = new Circle(10);
+	Circle circle = new Circle(10);
 	String expected = "A circle of radius 10.0";
 
 	// when
@@ -29,7 +29,7 @@ class ShapeTest {
     void should_ReturnInfo_When_SimpleSquareShape() {
 	
 	// given
-	Shape circle = new Square(10);
+	Square circle = new Square(10);
 	String expected = "A square of side 10.0";
 
 	// when
@@ -43,7 +43,7 @@ class ShapeTest {
     void should_ReturnInfo_When_ColoredCircle() {
 	
 	// given
-	Shape blueCircle = new ColoredShape<Circle>(() ->new Circle(11), "blue");
+	ColoredShape<Circle> blueCircle = new ColoredShape<>(() ->new Circle(11), "blue");
 	String expected = "A circle of radius 11.0 and blue color";
 	
 	// when
@@ -57,7 +57,7 @@ class ShapeTest {
     void should_ReturnInfo_When_ColoredSquare() {
 	
 	// given
-	Shape greenSquare = new ColoredShape<Square>(() ->new Square(11), "green");
+	ColoredShape<Square> greenSquare = new ColoredShape<>(() ->new Square(11), "green");
 	String expected = "A square of side 11.0 and green color";
 	
 	// when
@@ -72,7 +72,7 @@ class ShapeTest {
     void should_ReturnInfo_When_TransparentCircle() {
 	
 	// given
-	Shape transparentCircle = new TransparentShape<Circle>(() -> new Circle(11), 50);
+	TransparentShape<Circle> transparentCircle = new TransparentShape<>(() -> new Circle(11), 50);
 	String expected = "A circle of radius 11.0 and transparency of 50%";
 	
 	// when
@@ -86,7 +86,7 @@ class ShapeTest {
     void should_ReturnInfo_When_TransparentSquare() {
 	
 	// given
-	Shape transparentSquare = new TransparentShape<Square>(() -> new Square(11), 80);
+	TransparentShape<Square> transparentSquare = new TransparentShape<>(() -> new Square(11), 80);
 	String expected = "A square of side 11.0 and transparency of 80%";
 	
 	// when
@@ -100,7 +100,7 @@ class ShapeTest {
     void should_ReturnInfo_When_TransparentColoredCircle() {
 	
 	// given
-	Shape transparentBlueCircle = new TransparentShape<>(() -> 
+	TransparentShape<ColoredShape<Circle>> transparentBlueCircle = new TransparentShape<>(() -> 
 		new ColoredShape<>(() -> new Circle(8), "blue"), 12);
 	String expected = "A circle of radius 8.0 and blue color and transparency of 12%";
 	
@@ -115,7 +115,7 @@ class ShapeTest {
     void should_ResizeAndReturnInfo_When_TransparentColoredSquare() {
 
 	// given
-	Shape transparentGreenSquare = new TransparentShape<>(()->
+	TransparentShape<ColoredShape<Square>> transparentGreenSquare = new TransparentShape<>(()->
 		new ColoredShape<>(() -> new Square(8), "green"), 25);
 	String expected = "A square of side 16.0 and green color and transparency of 25%";
 	
@@ -131,7 +131,7 @@ class ShapeTest {
     void should_ResizeAndReturnInfo_When_ColoredTransparentCircle() {
 	
 	// given
-	Shape redTransparentCircle = new ColoredShape<>(() ->
+	ColoredShape<TransparentShape<Circle>> redTransparentCircle = new ColoredShape<>(() ->
 		new TransparentShape<>(() -> new Circle(8), 15), "red");
 	String expected = "A circle of radius 12.0 and transparency of 15% and red color";
 	
